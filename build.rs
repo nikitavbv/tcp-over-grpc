0141e742f1f215809e14552ead8ea373ab158739
Nikita Volobuev <nikitavbv@gmail.com>
2024-10-04 22:23:42 +0200

    feat(server): implement server

--- build.rs
+++ build.rs
@@ -4,6 +4,6 @@ fn main() -> Result<()> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("tcp_over_grpc_descriptor.bin"))
-        .compile(&["proto/tcp-over-grpc.proto"], &["proto"])?;
+        .compile_protos(&["proto/tcp-over-grpc.proto"], &["proto"])?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use {
     std::pin::Pin,
     tonic::{transport::Server, Status, Request, Streaming, Response},
-    tokio_stream::{Stream, StreamExt},
+    tokio_stream::{Stream, StreamExt, wrappers::ReceiverStream},
+    tokio::{net::TcpStream, io::{AsyncWriteExt, AsyncReadExt}, sync::mpsc},
     crate::rpc::{
         tcp_over_grpc_service_server::{TcpOverGrpcService, TcpOverGrpcServiceServer},
         IngressMessage,
@@ -34,6 +35,41 @@ impl TcpOverGrpcService for TcpOverGrpcHandler {
     type WrapTcpStreamStream = Pin<Box<dyn Stream<Item = Result<EgressMessage, Status>> + Send>>;
 
     async fn wrap_tcp_stream(&self, req: Request<Streaming<IngressMessage>>) -> Result<Response<Self::WrapTcpStreamStream>, Status> {
-        unimplemented!()
+        let mut req_stream = req.into_inner();
+        let target_stream = TcpStream::connect("tcpbin.com:4242").await.unwrap();
+        let (mut rx, mut tx) = target_stream.into_split();
+        let (res_tx, res_rx) = mpsc::channel(128);
+
+        tokio::spawn(async move {
+            while let Some(result) = req_stream.next().await {
+                let v = result.unwrap();
+                tx.write_all(&v.data).await.unwrap();
+            }
+        });
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                let n = rx
+                    .read(&mut buf)
+                    .await
+                    .unwrap();
+
+                if n == 0 {
+                    return;
+                }
+
+                res_tx.send(Ok(EgressMessage {
+                    data: buf[0..n].to_vec(),
+                })).await.unwrap();
+            }
+        });
+
+        let out_stream = ReceiverStream::new(res_rx);
+
+        Ok(Response::new(
+            Box::pin(out_stream) as Self::WrapTcpStreamStream
+        ))
     }
 }
